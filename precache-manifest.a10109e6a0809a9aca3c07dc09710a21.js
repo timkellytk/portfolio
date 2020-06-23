d1620883a154474a26f8b7fe214cac47fc0a5c97
Tim Kelly <timpkelly1@gmail.com>
2020-06-24 09:31:14 +1000

    Updates

---
+++ precache-manifest.a10109e6a0809a9aca3c07dc09710a21.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04178562375187084ce32cb862eb2605",
+    "url": "/index.html"
+  },
+  {
+    "revision": "068b99d614b5950ba2b4",
+    "url": "/static/css/main.d7f88581.chunk.css"
+  },
+  {
+    "revision": "36558210c22d92a05bab",
+    "url": "/static/js/2.febe66bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.febe66bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "068b99d614b5950ba2b4",
+    "url": "/static/js/main.0caaafc8.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "f06a223a6285409b576ca7c076c884d7",
+    "url": "/static/media/resume.f06a223a.pdf"
+  },
+  {
+    "revision": "a83630bcf76f11cb661f118cf6338127",
+    "url": "/static/media/tim-kelly-b&w-mobile.a83630bc.png"
+  },
+  {
+    "revision": "5678d488756e806396dc0d4fa33ad292",
+    "url": "/static/media/tim-kelly-b&w.5678d488.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.4f5c864558ba4764a8f39f2a4d962082.js"
+  "/precache-manifest.a10109e6a0809a9aca3c07dc09710a21.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
